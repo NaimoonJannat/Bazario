e29cb1f5cd396eb91c3a6c5c784094e182a533fe
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-16 20:32:19 +0600

    CreateCoupon added

--- src/Roles/Seller/Dashboard/DashboardPages/AddProduct.jsx
+++ src/Roles/Seller/Dashboard/DashboardPages/AddProduct.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddProduct = () => {
     return (
         <div>
-            <h2 className="text-5xl">Itna na mujhse tu pyaar barha</h2>
+            <h2 className="text-5xl">Add Product</h2>
         </div>
     );
 };
