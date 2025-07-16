edce2847a51a3d1a59f2f777e10fe84f4630dd6f
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-16 20:33:08 +0600

    User list page added

---
+++ src/Roles/Seller/Dashboard/DashboardPages/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserList = () => {
+    return (
+        <div>
+             <h2 className="text-5xl">User List</h2>
+        </div>
+    );
+};
+
+export default UserList;
