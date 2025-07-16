15841231ffcccc29c77ba30f12fff8b51b7a4e1c
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-16 20:36:50 +0600

    Expired products page added

---
+++ src/Roles/Seller/Dashboard/DashboardPages/ExpiredProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExpiredProducts = () => {
+    return (
+        <div>
+             <h2 className="text-5xl">Expired Products page</h2>
+        </div>
+    );
+};
+
+export default ExpiredProducts;
