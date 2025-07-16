8bcd6ccc059577242b3640ce036da7cd9b89659a
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-16 20:35:16 +0600

    Out of stock page added

---
+++ src/Roles/Seller/Dashboard/DashboardPages/OutofStock.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutofStock = () => {
+    return (
+        <div>
+             <h2 className="text-5xl">Out of Stock</h2>
+        </div>
+    );
+};
+
+export default OutofStock;
