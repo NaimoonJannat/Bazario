59755e247c34f96a068fbde5ab63ac6ceaff99c4
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-16 20:43:56 +0600

    on shop sale page

---
+++ src/Roles/Seller/Dashboard/DashboardPages/OnSHopSale.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OnSHopSale = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">On Shop Sale</h2>
+        </div>
+    );
+};
+
+export default OnSHopSale;
