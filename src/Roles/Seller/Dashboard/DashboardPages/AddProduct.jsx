cfe66b4fef3582b07154887c4aa6eabd9805a85d
NaimoonJannat <naimoonjannat@gmail.com>
2025-07-25 21:19:30 +0600

    Required input

--- src/Roles/Seller/Dashboard/DashboardPages/AddProduct.jsx
+++ src/Roles/Seller/Dashboard/DashboardPages/AddProduct.jsx
@@ -130,6 +130,7 @@ const handleIncrease = () => setStateQuantity((prev) => prev + 1);
                 <span className="text-[#001f3f] text-3xl">+</span>
                 <input
                   type="file"
+                  required
                   multiple
                   accept="image/*"
                   onChange={handleImageChange}
@@ -147,18 +148,18 @@ const handleIncrease = () => setStateQuantity((prev) => prev + 1);
             <div className="space-y-4">
               <div>
                 <label className="block mb-1 font-medium text-[#001f3f]">Product Name</label>
-                <input type="text" name="title" className="w-full text-[#001f3f] border rounded-lg px-4 py-2" placeholder="Product name" />
+                <input required type="text" name="title" className="w-full text-[#001f3f] border rounded-lg px-4 py-2" placeholder="Product name" />
               </div>
 
               <div>
                 <label className="block mb-1 font-medium text-[#001f3f]">Description</label>
-                <textarea name="description" className="w-full border text-[#001f3f] rounded-lg px-4 py-2" rows="5" placeholder="Product description"></textarea>
+                <textarea required name="description" className="w-full border text-[#001f3f] rounded-lg px-4 py-2" rows="5" placeholder="Product description"></textarea>
               </div>
 
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
                   <label className="block mb-1 font-medium text-[#001f3f]">Category</label>
-                  <input
+                  <input required
                     type="text"
                     name="category"
                     className="w-full border text-[#001f3f] rounded-lg px-4 py-2"
@@ -170,7 +171,7 @@ const handleIncrease = () => setStateQuantity((prev) => prev + 1);
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
                   <label className="block mb-1 font-medium text-[#001f3f]">Expiration Date</label>
-                  <input type="date" name="expireDate" className="w-full border text-[#001f3f] rounded-lg px-4 py-2" />
+                  <input required type="date" name="expireDate" className="w-full border text-[#001f3f] rounded-lg px-4 py-2" />
                 </div>
 
                 <div>
@@ -184,6 +185,7 @@ const handleIncrease = () => setStateQuantity((prev) => prev + 1);
                       −
                     </button>
                   <input
+                  required
   type="number"
   name="quantity"
   value={stateQuantity}
@@ -204,10 +206,10 @@ const handleIncrease = () => setStateQuantity((prev) => prev + 1);
 
               <div>
                 <label className="block mb-1 font-medium text-[#001f3f]">Price</label>
-                <input type="text" name="price" className="w-full text-[#001f3f] border rounded-lg px-4 py-2" placeholder="$0.00" />
+                <input required type="text" name="price" className="w-full text-[#001f3f] border rounded-lg px-4 py-2" placeholder="$0.00" />
               </div>
 
-              <input
+              <input required
                 className="btn btn-soft btn-success text-[#d4ff00] bg-[#001f3f]"
                 value={loading ? "Uploading..." : "Add Product"}
                 type="submit"
