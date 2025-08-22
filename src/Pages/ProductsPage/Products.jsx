dd41a5297b06cb0e1de425a83b2c88fbebd8c2a5
NaimoonJannat <naimoonjannat@gmail.com>
2025-08-22 19:41:23 +0600

    Product page working

--- src/Pages/ProductsPage/Products.jsx
+++ src/Pages/ProductsPage/Products.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ProductCard from './ProductCard';
 
 const Products = () => {
     return (
         <div>
             <h5 className='text-5xl'>Products page</h5>
-            <ProductCard></ProductCard>
+            {/* The container */}
+            <div className="w-full flex flex-col md:flex-row">
+
+                {/* Filter  */}
+                <div className="w-full max-h-screen md:w-1/3 border-4 border-white"></div>
+
+                {/* Products */}
+                <div className="w-full h-screen border-4 border-white md:w-1/2"></div>
+            </div>
         </div>
     );
 };
