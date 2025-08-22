1ec52be8889fac0a37508a0be8d781684db84041
NaimoonJannat <naimoonjannat@gmail.com>
2025-08-22 22:05:54 +0600

    Details page working

--- src/Pages/ProductsPage/ProductDetails.jsx
+++ src/Pages/ProductsPage/ProductDetails.jsx
@@ -3,13 +3,13 @@ import { useLoaderData, useParams } from "react-router";
 const ProductDetails = () => {
      const products = useLoaderData();
      const { id } = useParams();
-     const product = products.find((product) => product._id === id);
+     const product = products.find((task) => task._id === id);
     return (
         <div className="card lg:card-side lg:p-8">
             <div className="lg:w-1/2 rounded-2xl  flex justify-center items-center">
-                <img className="w-full border-2 border-[#d4ff00] shadow-2xl shadow-[#d4ff00] " src={product.image[0]} />
+                <img className="w-full border-2 border-[#d4ff00] shadow-2xl shadow-[#d4ff00] " src={product.images[0]} />
             </div>
-            <div className="card-body lg:w-1/2">
+            <div className="card-body lg:w-1/2 text-white">
                 <h2 className="text-4xl font-bold">{product.title}</h2>
                 <div className="divider"></div>
                 <div className="text-[#d4ff00] font-bold">{product.category}</div>
@@ -21,7 +21,7 @@ const ProductDetails = () => {
                     </p>
                 </div>
                 <div className="divider"></div>
-                <div className="flex gap-4">
+                <div className="flex gap-4 justify-center items-center">
                     <div>
                         <p className="text-base">Quantity: </p>
                         <p className="text-base">Expiry Date: </p>
