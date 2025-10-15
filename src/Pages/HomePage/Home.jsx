8236ece6e02134e23611c32a5e5c7dfd9d6ebcf4
NaimoonJannat <naimoonjannat@gmail.com>
2025-10-15 22:39:05 +0600

    Done some changes

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,40 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import HeroBanner from "./HeroBanner";
-import axios from "axios";
 import { FaArrowRight } from "react-icons/fa";
-import ProductCardH from "./ProductCardH";
 import FeedbackSection from "./FeedbackSection";
 
 // Swiper imports
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+
 
 import { useNavigate } from "react-router-dom";
+import Recommendation from "./RecommendationAI";
 
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const [recommended, setRecommended] = useState([]);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    axios.get("https://bazario-server-pearl.vercel.app/products").then((res) => {
-      const shuffled = res.data.sort(() => 0.5 - Math.random());
-      setProducts(shuffled.slice(0, 8));
-    });
-  }, []);
-
-  useEffect(() => {
-  const lastViewed = localStorage.getItem("lastViewedProductId");
-  if (lastViewed) {
-    axios
-      .get(`https://bazario-server-pearl.vercel.app/products/${lastViewed}/recommendations`)
-      .then((res) => setRecommended(res.data));
-  }
-}, []);
+  
 
 
   return (
@@ -42,67 +23,8 @@ const Home = () => {
       {/* Hero Section */}
       <HeroBanner />
 
-      {/* You May Like Section */}
-      {/* <section className="py-12 px-6">
-        <h2 className="text-3xl font-bold mb-8 text-[#d4ff00]">
-          You May Like
-        </h2>
-        <Swiper
-          modules={[Autoplay, Navigation]}
-          spaceBetween={20}
-          slidesPerView={1}
-          navigation
-          autoplay={{ delay: 2500, disableOnInteraction: false }}
-          breakpoints={{
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 4 },
-          }}
-        >
-          {products.map((product) => (
-            <SwiperSlide key={product._id}>
-              <div
-                onClick={() =>
-                  navigate(`/products/product/${product._id}`)
-                }
-                className="cursor-pointer"
-              >
-                <ProductCardH product={product} />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </section> */}
-
-      {/* Recommended For You */}
-{recommended.length > 0 && (
-  <section className="py-12 px-6">
-    <h2 className="text-3xl font-bold mb-8 text-[#d4ff00]">
-      Recommended For You
-    </h2>
-    <Swiper
-      modules={[Autoplay, Navigation]}
-      spaceBetween={20}
-      slidesPerView={1}
-      navigation
-      autoplay={{ delay: 2500, disableOnInteraction: false }}
-      breakpoints={{
-        640: { slidesPerView: 2 },
-        1024: { slidesPerView: 4 },
-      }}
-    >
-      {recommended.map((product) => (
-        <SwiperSlide key={product._id}>
-          <div
-            onClick={() => navigate(`/products/product/${product._id}`)}
-            className="cursor-pointer"
-          >
-            <ProductCardH product={product} />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </section>
-)}
+       {/* Recommendation section  */}
+        <Recommendation></Recommendation>
 
 
       {/* Categories Section */}
